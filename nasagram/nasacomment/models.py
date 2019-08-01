07a45656e49b585b2cb109244cbc5254f2334705
Kevin <ktwalsh@g.clemson.edu>
2019-08-01 09:10:40 -0400

    nasagram add

---
+++ nasagram/nasacomment/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.core.validators import URLValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Comment(models.Model):
+    image = models.TextField(validators=[URLValidator()])
+    comment = models.CharField(max_length = 300)
+    rating = models.IntegerField(default = 0,validators=[MaxValueValidator(5), MinValueValidator(0)])
